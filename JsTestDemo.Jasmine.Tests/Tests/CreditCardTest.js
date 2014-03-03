4d5a84d1aee383f447c95f78f53d55639832ab5c
David L <david.lindblad@hotmail.com>
2014-03-03 23:36:15 +0100

    Added Jasmine tests

---
+++ JsTestDemo.Jasmine.Tests/Tests/CreditCardTest.js
@@ -0,0 +1,21 @@
+﻿/// <chutzpah_reference path="../../JsTestDemo/app/dashboard/creditcard.js" />
+
+describe("Credit Card number", function () {
+    it("should return true when valid", function () {
+        expect(isCreditCardValid("1234567890123452")).toEqual(true);
+
+        expect(isCreditCardValid("4627383674323741")).toEqual(true);
+
+        expect(isCreditCardValid("7238562784627463")).toEqual(true);
+    });
+
+    it("should return false when invalid", function () {
+        expect(isCreditCardValid("4723847238643764")).toEqual(false);
+    });
+
+    it("should throw exception when number is invalid", function () {
+        expect(function () {
+            isCreditCardValid("ABCDEFG");
+        }).toThrow(new Error("Invalid credit card number"));
+    });
+})
