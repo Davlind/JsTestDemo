f691a6fe5f3efe6fd04f7fe575d2e58c1ab1ee08
David L <david.lindblad@hotmail.com>
2014-03-04 21:54:37 +0100

    changes

--- JsTestDemo.Jasmine.Tests/Tests/CreditCardTest.js
+++ JsTestDemo.Jasmine.Tests/Tests/CreditCardTest.js
@@ -2,20 +2,20 @@
 
 describe("Credit Card number", function () {
     it("should return true when valid", function () {
-        expect(isCreditCardValid("1234567890123452")).toEqual(true);
+        expect(app.creditCard.isValid("1234567890123452")).toEqual(true);
 
-        expect(isCreditCardValid("4627383674323741")).toEqual(true);
+        expect(app.creditCard.isValid("4627383674323741")).toEqual(true);
 
-        expect(isCreditCardValid("7238562784627463")).toEqual(true);
+        expect(app.creditCard.isValid("7238562784627463")).toEqual(true);
     });
 
     it("should return false when invalid", function () {
-        expect(isCreditCardValid("4723847238643764")).toEqual(false);
+        expect(app.creditCard.isValid("4723847238643764")).toEqual(false);
     });
 
     it("should throw exception when number is invalid", function () {
         expect(function () {
-            isCreditCardValid("ABCDEFG");
+            app.creditCard.isValid("ABCDEFG");
         }).toThrow(new Error("Credit card number must be a numeric value"));
     });
 })
--- JsTestDemo/app/dashboard/creditcard.js
+++ JsTestDemo/app/dashboard/creditcard.js
@@ -1,48 +1,42 @@
-﻿function isCreditCardValid(s) {
+﻿var app = {
+    creditCard: {
+        isValid: function(s) {
 
-    var i, n, c, r, t;
+            var i, n, c, r, t;
 
-    var reg = new RegExp('^\\d+$');
-    if (!reg.test(s)) {
-        throw Error("Credit card number must be a numeric value");
+            var reg = new RegExp('^\\d+$');
+            if (!reg.test(s)) {
+                throw Error("Credit card number must be a numeric value");
+            }
+
+            r = "";
+            for (i = 0; i < s.length; i++) {
+                c = parseInt(s.charAt(i), 10);
+                if (c >= 0 && c <= 9)
+                    r = c + r;
+            }
+
+            if (r.length !== 16)
+                throw Error("Credit card number is not of correct length");
+
+            t = "";
+            for (i = 0; i < r.length; i++) {
+                c = parseInt(r.charAt(i), 10);
+                if (i % 2 != 0)
+                    c *= 2;
+                t = t + c;
+            }
+
+            n = 0;
+            for (i = 0; i < t.length; i++) {
+                c = parseInt(t.charAt(i), 10);
+                n = n + c;
+            }
+
+            if (n != 0 && n % 10 == 0)
+                return true;
+            else
+                return false;
+        }
     }
-    
-    r = "";
-    for (i = 0; i < s.length; i++) {
-        c = parseInt(s.charAt(i), 10);
-        if (c >= 0 && c <= 9)
-            r = c + r;
-    }
-
-    // Check for invalid length.
-
-    if (r.length !== 16)
-        throw Error("Credit card number is not of correct length");
-
-    // Now run through each single digit to create a new string. Even digits
-    // are multiplied by two, odd digits are left alone.
-
-    t = "";
-    for (i = 0; i < r.length; i++) {
-        c = parseInt(r.charAt(i), 10);
-        if (i % 2 != 0)
-            c *= 2;
-        t = t + c;
-    }
-
-    // Finally, add up all the single digits in this string.
-
-    n = 0;
-    for (i = 0; i < t.length; i++) {
-        c = parseInt(t.charAt(i), 10);
-        n = n + c;
-    }
-
-    // If the resulting sum is an even multiple of ten (but not zero), the
-    // card number is good.
-
-    if (n != 0 && n % 10 == 0)
-        return true;
-    else
-        return false;
 }
