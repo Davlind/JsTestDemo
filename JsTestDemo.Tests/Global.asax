e128e195c1b66679e925b568617136ef92db6476
David L <david.lindblad@hotmail.com>
2014-03-05 22:19:16 +0100

    Added tests

---
+++ JsTestDemo.Tests/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JsTestDemo.Tests.MvcApplication" Language="C#" %>
---
+++ JsTestDemo.Tests/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JsTestDemo.Tests.Startup))]
+namespace JsTestDemo.Tests
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
