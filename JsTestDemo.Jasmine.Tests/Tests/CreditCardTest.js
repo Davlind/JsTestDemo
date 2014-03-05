dc6d38d7998310eee0dd63aa1571f2d795c9cee4
David L <david.lindblad@hotmail.com>
2014-03-05 21:27:36 +0100

    misc

--- JsTestDemo.Jasmine.Tests/Tests/CreditCardTest.js
+++ JsTestDemo.Jasmine.Tests/Tests/CreditCardTest.js
@@ -1,21 +1,19 @@
 ﻿/// <chutzpah_reference path="../../JsTestDemo/app/dashboard/creditcard.js" />
 
-describe("Credit Card number", function () {
-    it("should return true when valid", function () {
-        expect(app.creditCard.isValid("1234567890123452")).toEqual(true);
-
+describe("Credit card", function () {
+    it("is valid when the luhn check is ok", function () {
         expect(app.creditCard.isValid("4627383674323741")).toEqual(true);
 
         expect(app.creditCard.isValid("7238562784627463")).toEqual(true);
     });
 
-    it("should return false when invalid", function () {
+    it("is invalid when the luhn check fails", function () {
         expect(app.creditCard.isValid("4723847238643764")).toEqual(false);
     });
 
     it("should throw exception when number is invalid", function () {
-        expect(function () {
+        expect(function() {
             app.creditCard.isValid("ABCDEFG");
         }).toThrow(new Error("Credit card number must be a numeric value"));
     });
-})
+});
